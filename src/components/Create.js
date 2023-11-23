d82ecde4a5ca2bcde354cffbd1f3c168cfaa4c61
ShahidUllahSE <shahidbangash8899@gmail.com>
2023-11-23 12:13:56 +0500

    created a todo app in react only

---
+++ src/components/Create.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <div>
+      <input type='text' name='' id=''/>
+      <button type='button'> Add </button>
+    </div>
+  )
+}
+
+export default Create
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Create from './Create'
+
+const Home = () => {
+  return (
+    <div>
+      
+      <h2>TODO List</h2>
+      <Create/>
+
+    </div>
+  )
+}
+
+export default Home
